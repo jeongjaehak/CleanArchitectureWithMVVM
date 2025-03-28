8fbf8879e83152dae5fc231ed15f748c4f0d47c3
정재학 <hika@jeongjaehag-ui-Macmini.local>
2025-03-28 14:16:09 +0900

    test: 네트워크 작동 여부 확인 하기 위한 코드

--- Projects/Views/Sources/ListView.swift
+++ Projects/Views/Sources/ListView.swift
@@ -5,10 +5,28 @@
 //  Created by 정재학 on 3/27/25.
 //
 
+import Combine
+import Entities
+import NetworkFramework
 import SwiftUI
 
 public struct ListView: View {
-    public init() {}
+    var test = Set<AnyCancellable>()
+    
+    public init() {
+        
+        APIUsers().request().sink { completion in
+            switch completion {
+            case .finished:
+                print( "성공" )
+            case .failure( let error ):
+                print( "error: \(error.localizedDescription)" )
+            }
+        } receiveValue: { userInfo in
+            print( "userInfo: \(userInfo)" )
+        }.store(in: &test)
+
+    }
 
     public var body: some View {
         Text("Hello, World!")
