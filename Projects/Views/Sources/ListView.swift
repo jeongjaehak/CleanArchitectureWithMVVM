a4ba0b749d6282e0edbe9d73e161b9ed0cbc2620
정재학 <hika@jeongjaehag-ui-Macmini.local>
2025-04-02 20:31:06 +0900

    refactor: 리스트화면 코드정리

--- Projects/Views/Sources/ListView.swift
+++ Projects/Views/Sources/ListView.swift
@@ -15,50 +15,46 @@ public struct ListView: View {
     
     // MARK: - Properties
     
-    private var cancellables = Set<AnyCancellable>()
-    private let input = PassthroughSubject<ListViewModel.Input, Never>()
     private var viewModel = ListViewModel()
+    private let input = PassthroughSubject<ListViewModel.Input, Never>()
     @State private var userInformations = [UserInformation]()
     
     // MARK: - Init
     
     public init() {}
-    
+
     // MARK: - Body
     
     public var body: some View {
         NavigationView {
-            List() {
-                Section(header: Text("정재학의 포트폴리오")) {
-                    //                    List(userInformations, id:\.self) { userInfo in
-                    ForEach(userInformations, id: \.id) { userInfo in
-                        NavigationLink {
-                            // 터치 시 상세화면 진입
-                            DetailView(userInformation: userInfo)
-                            
-                        } label: {
-                            // 각 셀에서 보여줄 정보화면
-                            ListViewCell(userInformation: userInfo)
-                        }
-                    }
+            List(userInformations, id: \.id) { userInfo in
+                NavigationLink(destination: DetailView(userInformation: userInfo)) {
+                    ListViewCell(userInformation: userInfo)
                 }
             }
+            .listStyle(.grouped)
+            .navigationTitle("정재학의 포트폴리오")
         }
-        .clipped() // clipToBounds
-        .scrollContentBackground(.hidden) // 스크롤 배경 숨김
-        .background(Color.white)    // 배경 흰색 처리
-        .onAppear() {
-            // 서버에서 데이터 로드
-            input.send(.loadUsers)
-        }
+        .clipped()
+        .scrollContentBackground(.hidden)
+        .background(Color.white)
+        .onAppear(perform: loadUsers)
         .onReceive(viewModel.transform(input: input.eraseToAnyPublisher())) { event in
-            switch event {
-                // 새로운 데이터가 들어오면 갱신한다.
+            switch event {   
             case .updateList(users: let users):
                 userInformations = users
             }
         }
     }
+}
+
+// MARK: - Private Functions
+
+extension ListView {
+    private func loadUsers() {
+        input.send(.loadUsers)
+    }
+
 }
 
 // MARK: - Preview
